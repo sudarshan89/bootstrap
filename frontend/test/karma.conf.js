e8dfa4551eefcbc9702c52f01ef0f1da0d5ccf41
shivarajkv <shivaraj.kv05@gmail.com>
2014-07-23 18:35:11 +0530

    config

--- frontend/test/karma.conf.js
+++ frontend/test/karma.conf.js
@@ -4,68 +4,91 @@
 // generator-karma 0.8.2
 
 module.exports = function(config) {
-  config.set({
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+    config.set({
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: true,
 
-    // base path, that will be used to resolve files and exclude
-    basePath: '../',
+        // base path, that will be used to resolve files and exclude
+        //basePath: '',
 
-    // testing framework to use (jasmine/mocha/qunit/...)
-    frameworks: ['jasmine'],
+        // testing framework to use (jasmine/mocha/qunit/...)
+        frameworks: ['jasmine'],
 
-    // list of files / patterns to load in the browser
-    files: [
-      'bower_components/angular/angular.js',
-      'bower_components/angular-mocks/angular-mocks.js',
-      'bower_components/angular-cookies/angular-cookies.js',
-      'bower_components/angular-resource/angular-resource.js',
-      'bower_components/angular-sanitize/angular-sanitize.js',
-      'bower_components/angular-touch/angular-touch.js',
-      'app/scripts/**/*.js',
-      'test/mock/**/*.js',
-      'test/spec/**/*.js'
-    ],
+        preprocessors: {
+            '../app/views/**/*.html': ['ng-html2js'],
+            '../app/*.html':['ng-html2js']
+        },
+        ngHtml2JsPreprocessor: {
+            cacheIdFromPath: function(filepath) {
+                return filepath.replace(/^.*app\//,'../');
+            }
+        },
 
-    // list of files / patterns to exclude
-    exclude: [],
+        // list of files / patterns to load in the browser
+        files: [
+            '../bower_components/jquery/dist/jquery.js',
+            '../bower_components/angular/angular.js',
+            '../bower_components/angular-mocks/angular-mocks.js',
+            '../bower_components/angular-cookies/angular-cookies.js',
+            '../bower_components/angular-resource/angular-resource.js',
+            '../bower_components/angular-sanitize/angular-sanitize.js',
+            '../bower_components/angular-touch/angular-touch.js',
+            '../bower_components/angular-strap/dist/angular-strap.js',
+            '../bower_components/angular-strap/dist/angular-strap.tpl.js',
+            '../bower_components/angular-route/angular-route.js',
+            '../app/views/**/*.html',
+            '../app/*.html',
+            '../app/scripts/**/*.js',
+            '../test/spec/mocks/**/*.js',
+            '../test/spec/**/*.js',
+            '../test/spec/*.js',
+            '../bower_components/bootstrap/dist/css/*.css',
+            '../bower_components/bootstrap/dist/js/*.js'
+        ],
 
-    // web server port
-    port: 8080,
+        // list of files / patterns to exclude
+        exclude: [],
 
-    // Start these browsers, currently available:
-    // - Chrome
-    // - ChromeCanary
-    // - Firefox
-    // - Opera
-    // - Safari (only Mac)
-    // - PhantomJS
-    // - IE (only Windows)
-    browsers: [
-      'PhantomJS'
-    ],
+        // web server port
+        port: 8080,
 
-    // Which plugins to enable
-    plugins: [
-      'karma-phantomjs-launcher',
-      'karma-jasmine'
-    ],
+        // Start these browsers, currently available:
+        // - Chrome
+        // - ChromeCanary
+        // - Firefox
+        // - Opera
+        // - Safari (only Mac)
+        // - PhantomJS
+        // - IE (only Windows)
+        browsers: [
+           'PhantomJS'
+        ],
 
-    // Continuous Integration mode
-    // if true, it capture browsers, run tests and exit
-    singleRun: false,
+        // Which plugins to enable
+        plugins: [
+            'karma-phantomjs-launcher',
+            'karma-jasmine',
+            'karma-ng-html2js-preprocessor',
+            'karma-chrome-launcher',
+            'karma-firefox-launcher',
+            'karma-ie-launcher'
+        ],
 
-    colors: true,
+        // Continuous Integration mode
+        // if true, it capture browsers, run tests and exit
+        singleRun: false,
 
-    // level of logging
-    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-    logLevel: config.LOG_INFO,
+        colors: true,
 
-    // Uncomment the following lines if you are using grunt's server to run the tests
-    // proxies: {
-    //   '/': 'http://localhost:9000/'
-    // },
-    // URL root prevent conflicts with the site root
-    // urlRoot: '_karma_'
-  });
+        // level of logging
+        // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+        logLevel: config.LOG_INFO
+
+        // Uncomment the following lines if you are using grunt's server to run the tests
+        // proxies: {
+        //   '/': 'http://localhost:9000/'
+        // },
+        // URL root prevent conflicts with the site root
+        // urlRoot: '_karma_'
+    });
 };
