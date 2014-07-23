60046db3ccd548ab595130c1fa2c4d229dd9fab5
shivarajkv <shivaraj.kv05@gmail.com>
2014-07-23 18:33:03 +0530

    updates

--- frontend/app/scripts/directives/directives.js
+++ frontend/app/scripts/directives/directives.js
@@ -1,12 +1,15 @@
-angular.module('entrancebook.directives', ['mgcrea.ngStrap'])
-.directive('loginWindow',['$modal',function($modal){
+'use strict';
+angular.module('entrancebook.directives', ['mgcrea.ngStrap.modal','mgcrea.ngStrap.alert'])
+.directive('loginWindow',['$modal','$alert',function($modal,$alert){
         return{
             restrict: 'A',
-            link: function(scope, elem, attrs) {
+            link: function(scope) {
                 var modal = $modal({ template: '../views/login/login.html',show:false});
-                scope.$on('auth-invalid-user', function() {
+                var alert;
+                scope.$on('auth-invalid-user', function(event,invalidStatus) {
                     modal.$promise.then(modal.show);
+                    alert =  $alert({content: invalidStatus, type: 'danger', show: true,container:'#invalid-credential'});
                 });
             }
-        }
+        };
     }]);
